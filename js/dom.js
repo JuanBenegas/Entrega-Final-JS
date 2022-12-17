e4754bd547de2695bd2430e6cc326191094a8701
JuanBenegas <juan.benegas1320@gmail.com>
2022-12-16 23:06:04 -0300

    Segundo commit

--- js/dom.js
+++ js/dom.js
@@ -2,6 +2,7 @@ const carrito = []
 const titulo = document.getElementById("titulo")
 const container = document.getElementById("card-container")
 const footer = document.getElementById("footer")
+const productos = []
 
 titulo.innerText = "Fantasía automotriz"
 footer.innerText = "Copyrigth-2022-No tengo ideas"
@@ -17,3 +18,14 @@ function mostrarCards(array) {
 }
 
 mostrarCards(coches)
+
+function clickBotones() {
+    const botonesTienda = document.querySelectorAll("button.btn-compra")
+    botonesTienda.forEach(btn => {
+        btn.addEventListener("click", () => {
+            let resultado = productos.find(prod => prod.id === parseInt(btn.id))
+            console.log(resultado)
+            localStorage.setItem("miCarrito", JSON.stringify(carrito))
+        })
+    })
+}
